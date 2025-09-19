ad6d680f04314579649d49f5800eb7b9fadfd440
tim-neville <nikefootbag@gmail.com>
2025-09-19 13:17:20 +1000

    Add frame time limiter and deleta time calc to game loop

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,9 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(30) / 1000 #convert  milliseconds to seconds
+
 
 if __name__ == "__main__":
     main()
